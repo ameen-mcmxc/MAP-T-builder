095b841191fccac2c103314bc589c462d3caae09
Ameen Al-Azzawi <45686881+ameen-mcmxc@users.noreply.github.com>
2023-03-05 21:49:45 +0100

    Create infor-disclosure.py

---
+++ infor-disclosure.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from scapy.all import *
+
+
+# This scripts prints out the payload content of the sniffed UDP or TCP Packet and also the data inside the ICMPv6 packet.
+def packet_handler(packet):
+        if UDP in packet:
+            udp_payload = packet[UDP].payload
+            print(f"UDP Packet:")
+            print(f"Payload: {udp_payload}")
+        elif ICMPv6EchoRequest in packet:
+            icmp6_payload = packet[ICMPv6EchoRequest].data
+            print(f"ICMPv6 Echo Request Packet:")
+            print(f"Payload: {icmp6_payload}")
+        elif TCP in packet:
+            tcp_payload = packet[TCP].payload
+            print(f"TCP Payload: {tcp_payload}")
+sniff(iface='ens34', filter='udp or icmp6 or tcp', prn=packet_handler)
