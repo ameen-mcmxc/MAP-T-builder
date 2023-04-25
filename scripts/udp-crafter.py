bbcc480847d7a275c6d7ef15565be4c28a2428eb
Ameen Al-Azzawi <45686881+ameen-mcmxc@users.noreply.github.com>
2023-04-25 10:32:14 +0200

    Update udp-crafter.py

--- scripts/udp-crafter.py
+++ scripts/udp-crafter.py
@@ -3,12 +3,27 @@
 # This script is used to send UDP packet from the client to the Server.
 
 from scapy.all import *
+import cryptography
 
 # Create the packet
 ip = IP(dst="192.0.2.2")
-udp = UDP(dport=80)
-payload = load=("From IPv4 Cleint with Love")
+udp = UDP(dport=22)
+payload = load=("I'm the payload of the UDP packet")
 pkt = ip/udp/payload
 
 # Send the packet
 send(pkt)
+
+
+
+## OR
+# echo "Your message" | nc -u 192.0.2.2 22
+
+## OR
+
+# echo "Your message" | socat - UDP-DATAGRAM:192.0.2.2:22
+
+## OR
+# From specific source port: 5000
+
+# hping3 -c 1 -2 -s 5000 -p 22 192.0.2.2
